22c810a2dd4c0a87b636eababbb09a344159aa15
joeostrander <ostrander.jm@gmail.com>
2017-02-10 12:41:54 -0600

    initial commit

---
+++ ZipCompare/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxZipMaster = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxZipTest = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonCompare = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderEntry = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(416, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxZipMaster
+            // 
+            this.textBoxZipMaster.Location = new System.Drawing.Point(12, 34);
+            this.textBoxZipMaster.Name = "textBoxZipMaster";
+            this.textBoxZipMaster.Size = new System.Drawing.Size(398, 20);
+            this.textBoxZipMaster.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Master Zip File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Test Zip File:";
+            // 
+            // textBoxZipTest
+            // 
+            this.textBoxZipTest.Location = new System.Drawing.Point(12, 96);
+            this.textBoxZipTest.Name = "textBoxZipTest";
+            this.textBoxZipTest.Size = new System.Drawing.Size(398, 20);
+            this.textBoxZipTest.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(416, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonCompare
+            // 
+            this.buttonCompare.Location = new System.Drawing.Point(12, 132);
+            this.buttonCompare.Name = "buttonCompare";
+            this.buttonCompare.Size = new System.Drawing.Size(75, 23);
+            this.buttonCompare.TabIndex = 6;
+            this.buttonCompare.Text = "&Compare";
+            this.buttonCompare.UseVisualStyleBackColor = true;
+            this.buttonCompare.Click += new System.EventHandler(this.buttonCompare_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderEntry,
+            this.columnHeaderStatus});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(15, 177);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(641, 389);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderEntry
+            // 
+            this.columnHeaderEntry.Text = "Entry";
+            this.columnHeaderEntry.Width = 375;
+            // 
+            // columnHeaderStatus
+            // 
+            this.columnHeaderStatus.Text = "Status";
+            this.columnHeaderStatus.Width = 250;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(83, 26);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(82, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 578);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.buttonCompare);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxZipTest);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxZipMaster);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(483, 617);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBoxZipMaster;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxZipTest;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonCompare;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderEntry;
+        private System.Windows.Forms.ColumnHeader columnHeaderStatus;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ ZipCompare/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Compression;
+//using System.IO.Compression.FileSystem;
+using System.Collections.Generic;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = Application.ProductName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select Master Zip File";
+            openFileDialog1.Filter = "Zip Files (*.zip)|*.zip";
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                textBoxZipMaster.Text = openFileDialog1.FileName;
+            }
+            
+        }
+
+        private void buttonCompare_Click(object sender, EventArgs e)
+        {
+            if (textBoxZipMaster.Text.ToLower()==textBoxZipTest.Text.ToLower())
+            {
+                MessageBox.Show("You must select 2 DIFFERENT files!", "Select different files", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(textBoxZipMaster.Text))
+                return;
+
+            if (string.IsNullOrEmpty(textBoxZipTest.Text))
+                return;
+
+
+            string zipMaster = textBoxZipMaster.Text;
+            string zipTest = textBoxZipTest.Text;
+
+            listView1.Items.Clear();
+
+            List<ZipArchiveEntry> masterList = new List<ZipArchiveEntry>();
+
+            
+            using (ZipArchive archiveMaster = ZipFile.Open(zipMaster, ZipArchiveMode.Read))
+            {
+                Console.WriteLine("Master entry count:  {0}", archiveMaster.Entries.Count);
+                foreach (ZipArchiveEntry entry in archiveMaster.Entries)
+                {
+                    masterList.Add(entry);
+                }
+
+            }
+
+            using (ZipArchive archiveTest = ZipFile.Open(zipTest, ZipArchiveMode.Read))
+            {
+                Console.WriteLine("Test entry count:  {0}",archiveTest.Entries.Count);
+
+                foreach(ZipArchiveEntry entry in masterList)
+                {
+                    var testEntry = archiveTest.GetEntry(entry.FullName);
+                    if (testEntry != null)
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        lvi.Text = entry.FullName;
+
+                        String status = "";
+                        if (testEntry.Length != entry.Length)
+                        {
+                            status += "Size,";
+                            Console.WriteLine("Bad length:  " + testEntry.Length + " != " + entry.Length);
+                        }
+
+                        if (testEntry.LastWriteTime != entry.LastWriteTime)
+                        {
+                            status += "LastWriteTime,";
+                            Console.WriteLine("Bad LastWriteTime:  " + testEntry.LastWriteTime + " != " + entry.LastWriteTime);
+                        }
+
+                        if (testEntry.CompressedLength != entry.CompressedLength)
+                        {
+                            status += "Compressed Size";
+                            Console.WriteLine("Bad CompressedLength:  " + testEntry.CompressedLength + " != " + entry.CompressedLength);
+                        }
+
+
+
+                        if (string.IsNullOrEmpty(status))
+                        {
+                            status = "OK";
+                        } else
+                        {
+                            status = "BAD:  " + status;
+                            if (status.EndsWith(","))
+                            {
+                                status = status.Remove(status.Length-1);
+                            }
+
+                        }
+
+                            
+
+                        lvi.SubItems.Add(status);
+                        listView1.Items.Add(lvi);
+                    }
+                    else
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        lvi.Text = entry.FullName;
+                        lvi.SubItems.Add("MISSING");
+                        listView1.Items.Add(lvi);
+                    }
+                    
+                }
+                
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select Test Zip File";
+            openFileDialog1.Filter = "Zip Files (*.zip)|*.zip";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxZipTest.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 ab = new AboutBox1();
+            ab.ShowDialog();
+        }
+    }
+
+ 
+
+    
+}
+
